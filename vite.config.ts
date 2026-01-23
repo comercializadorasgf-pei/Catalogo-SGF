46101415dd98719f22aa5fdca7f74cd1856f09a6
comercializadorasgf-pei <comercializadora.sgf@gmail.com>
2026-01-23 08:27:19 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/Catalogo-SGF/', // 
+    base: '/Catalogo-SGF/',   // 
     server: {
       port: 3000,
       host: '0.0.0.0',
@@ -14,13 +14,14 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
     },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
-      },
-    },
+      }
+    }
   };
 });
+
 
